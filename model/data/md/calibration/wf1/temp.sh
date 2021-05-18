5c49c501e24c18e233c5c658393aa18814c429d8
Gianluca Turin <>
2021-05-18 13:27:24 +0200

    replace colon with underscore in csv file names

---
+++ model/data/md/calibration/wf1/temp.sh
@@ -0,0 +1,21 @@
+for xx in `find . -mindepth 1 -maxdepth 1`
+do
+  #echo $xx
+  if [ -d "$xx" ]
+  then
+    for x in `find $xx -mindepth 1 -maxdepth 1`
+    do
+      if [ -d "$x" ]
+      then
+        echo "Dir $x remains to be checked"
+      else
+        echo "File $x needs to be renamed"
+        echo "renamed from: $x"
+        newx=`echo "$x" | sed 's/:/_/g'`
+        mv $x $newx
+        echo "to $newx" 
+      fi
+    done
+  fi
+  #mv -- "$x" "${x// /_}"
+done
