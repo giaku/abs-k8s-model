0e2f791075e7cb6b2008304b9f2d5ac179977120
borg-a <borga.a92@gmail.com>
2021-05-24 18:25:37 +0200

    added simulations and plots for 3wf mixedwfs

--- model/data/scripts/runMixed.py
+++ model/data/scripts/runMixed.py
@@ -6,11 +6,46 @@ Created on Wed May 19 18:42:18 2021
 """
 import os
 import mixedPlot as mxp
+import sys
 
-paths = ['..\\md\\mixed_wf']
-for path in paths:
-    for fld in os.listdir(path):
-        try:
-            mxp.run(path + '\\' + fld)
-        except:
-            print('error running on: '  + path + '\\' + fld)
+def runAll():
+    paths = ['..\\md\\mixed_wf']
+    for path in paths:
+        for fld in os.listdir(path):
+            try:
+                mxp.run(path + '\\' + fld)
+            except:
+                print('error running on: '  + path + '\\' + fld)
+                
+def runNew():
+    paths = ['..\\md\\mixed_wf']
+    executed = ''
+    for path in paths:
+        for fld in os.listdir(path):
+            try:
+                pth = path + '\\' + fld
+                files = os.listdir(pth)
+                svg = False
+                for f in files:
+                    if f.endswith('.svg'):
+                        svg = True
+                if not svg:
+                    try:
+                        mxp.run(pth)
+                        executed = executed + pth + '\n'
+                    except:
+                        print('error running on: '  + path + '\\' + fld)
+            except:
+                pass
+    
+    print('Executed: \n' + executed)
+
+if len(sys.argv) > 1:
+    if sys.argv[1] == '-all' and len(sys.argv) == 2:
+        runAll()
+    else:
+        raise Exception('invalid arguments')
+else:
+    runNew()
+    
+
