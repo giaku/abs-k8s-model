182886168bf72026a5ecd95c61be120773ca14f6
Gianluca Turin <gianluca.turin@gmail.com>
2021-11-15 10:35:43 +0100

    added experiments of triplets with default scheduling and couples with 2A2C scheduling

--- model/abse.sh
+++ model/abse.sh
@@ -1,3 +1,3 @@
-absc -e tests/fixed-nodes/triplets/$1.abs K8sMaster.abs K8sService.abs K8sUtil.abs K8sWorkflow.abs K8sClients.abs
+absc -e tests/fixed-nodes/scheduling-2A-2C/couples/$1.abs K8sMaster.abs K8sService.abs K8sUtil.abs K8sWorkflow.abs K8sClients.abs
 
 ./run.sh $1
--- model/run.sh
+++ model/run.sh
@@ -1,6 +1,6 @@
 gen/erl/run -l 110 > outputs/$1.txt
 
-cp outputs/$1.txt data/md/fixed-nodes/$1/model.txt
+cp outputs/$1.txt data/md/fixed-nodes/scheduling-2A-2C/$1/model.txt
 
-python3 data/scripts/mixedPlot.py data/md/fixed-nodes/$1
+python3 data/scripts/mixedPlot.py data/md/fixed-nodes/scheduling-2A-2C/$1
 #python3 plot_scripts/plot.py outputs/$1.txt
